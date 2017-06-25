202379d9a2ae52dd0e9eab773a8767468dc86c62
sidtuli <siddytuli11@gmail.com>
2017-06-25 10:25:03 -0400

    Some example stuff

--- demo/demo.py
+++ demo/demo.py
@@ -1,13 +1,29 @@
 from PIL import Image
-im = Image.open('test_image.svg','r')
-width, height = im.size
-pixel_values = list(im.getdata())
+import os
 
-color_dict = {}
-for color in pixel_values:
-    if color in color_dict:
-        color_dict[color] += 1
-    else :
-        color_dict[color] = 1
 
-print(color_dict)
+def get_color(file_path) :
+    im = Image.open(file_path,'r')
+    if im.mode != "RGB":
+        im = im.convert('RGB')
+    pixel_values = list(im.getdata())
+
+    color_dict = {}
+    for color in pixel_values:
+        if color in color_dict:
+            color_dict[color] += 1
+        else :
+            color_dict[color] = 1
+    
+    print(im.mode)  
+    print(color_dict,"\n")
+
+def process_dict(dir_path) :
+    print(os.listdir(os.getcwd()+dir_path))
+    for filename in os.listdir(os.getcwd()+dir_path):
+        file_path = os.getcwd()+dir_path+"/"+filename
+        get_color(file_path)
+        
+
+#get_color("test_image.svg")
+process_dict("/flags-normal")
