1cccdf23b4a39ce14886755c233f2243cea3bd34
sidtuli <stuli@terpmail.umd.edu>
2017-06-29 22:20:27 -0400

    Attempting to write out info to text files

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A little project to analyze the colors of images.
 ###### Technology Used
 * Python 3.6.1 [link](https://www.python.org/downloads/release/python-361/) 
 * Pillow 4.1.1 [package link](https://pypi.python.org/pypi/Pillow/4.1.1), [official docs](http://python-pillow.org/)
+
+##### Flags/Demo Data
+* flagpedia [link](http://flagpedia.net/download)
--- demo/demo.py
+++ demo/demo.py
@@ -4,8 +4,8 @@ import os
 
 def get_color(file_path) :
     im = Image.open(file_path,'r')
-    if im.mode != "RGB":
-        im = im.convert('RGB')
+    if im.mode != "RGBA":
+        im = im.convert('RGBA')
     pixel_values = list(im.getdata())
 
     color_dict = {}
@@ -14,15 +14,24 @@ def get_color(file_path) :
             color_dict[color] += 1
         else :
             color_dict[color] = 1
-    
-    print(im.mode)  
-    print(color_dict,"\n")
+    color_dict_str = ""
+    for (color, count) in color_dict.items():
+        color_dict_str += str(color) + " - " + str(count) +","
+    #print(im.mode)  
+    #print(color_dict,"\n")
+    return color_dict_str
 
 def process_dict(dir_path) :
-    print(os.listdir(os.getcwd()+dir_path))
+    #print(os.listdir(os.getcwd()+dir_path))
+    file = open("test.txt","w") 
     for filename in os.listdir(os.getcwd()+dir_path):
         file_path = os.getcwd()+dir_path+"/"+filename
-        get_color(file_path)
+        res = get_color(file_path)
+        file.write(filename + " - ")
+        file.write(res)
+        file.write('\n')
+
+    file.close()
         
 
 #get_color("test_image.svg")
