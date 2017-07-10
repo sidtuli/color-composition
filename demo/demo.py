e49aabac493ad87a03923ea19a8c2cd9800288e9
sidtuli <stuli@terpmail.umd.edu>
2017-07-09 20:46:01 -0400

    Restacking is done
    I found a way to "sort" the pixels of animage and then return them in a new image. Also I hopefully made some better efficiency changes.

--- demo/demo.py
+++ demo/demo.py
@@ -3,12 +3,27 @@ import os, json
 
 
 def get_color(file_path) :
-    im = Image.open(file_path,'r')
+    im = Image.open(file_path,'r').copy()
     if im.mode != "RGBA":
         im = im.convert('RGBA')
     
-    return im.getcolors(maxcolors=len(im.getdata()))
+    result = im.getcolors(maxcolors=len(im.getdata()))
+    im.close
+    return result
 
+def restackimage(original_img):
+    im = Image.open(original_img,'r').copy()
+    if im.mode != "RGBA":
+        im = im.convert('RGBA')
+    pixellist = list(im.getdata())
+    newpixels = list()
+    for pixel in reversed(sorted(pixellist)):
+        newpixels.append(pixel)
+
+    restack = Image.new(im.mode,im.size)
+    restack.putdata(newpixels)
+    restack.save(original_img+"_restacked.png")
+    
 def process_dict(dir_path) :
     
     file = open("test.json","w")
@@ -24,5 +39,6 @@ def process_dict(dir_path) :
         
 
 
-process_dict("/test_images")
+#process_dict("/test_images")
+restackimage(os.getcwd()+"/test_images/stop_thief.png")
 
