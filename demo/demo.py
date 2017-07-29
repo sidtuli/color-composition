4ee14daeca6deb0f70e79c87be05675766694fb1
sidtuli <stuli@terpmail.umd.edu>
2017-07-28 21:05:56 -0400

    Code clean up

--- demo/demo.py
+++ demo/demo.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import os, json
 
-# **** Functions to process files and directories ****
+# **********************************************************************************************************************
+#                                       **** Functions to process files and directories ****
+# **********************************************************************************************************************
 
 def process_dict(dir_path) :
     '''
-    Goes through an entire directory to output a data file of 
+    Goes through an entire directory to output a data file of all files within them
     '''
     json_file = open("test.json","w")
     data = {}
@@ -14,14 +16,9 @@ def process_dict(dir_path) :
     i = 0
     file_paths = []
     for root, dirs, files in stuff:
-        '''print(root)
-        print(dirs)
-        print(files)
-        print("*****************************************")'''
         data_prepend = root.split(os.sep)[-1]
-        
         file_paths = file_paths + [(root+"/"+file,data_prepend+"/"+file) for file in files]
-    #print(file_paths)
+    
     for file_path in file_paths:
         data_name = file_path[1]
         data[data_name] = process_file(file_path[0])
@@ -31,10 +28,71 @@ def process_dict(dir_path) :
     json_file.close()
 
 def process_file(file_path):
-    if can_open_image(file_path) and not is_multi_frame(file_path):
-        return getcolor(file_path)
+    '''
+    Function to determine what is done with a specified file
+    '''
+    if can_open_image(file_path):
+        if is_multi_frame(file_path):
+            return getcolorgif(file_path)
+        else:
+            return getcolor(file_path)
     else:
         return "null"
+
+# **********************************************************************************************************************
+#                   **** Utility functions - checking properties of files and other useful methods ****
+# **********************************************************************************************************************
+
+def can_open_image(img_path):
+    '''
+    A function to check if file can be opened by PIL library
+    '''
+    try:
+        im = Image.open(img_path)
+        im.close()
+        return True
+    except OSError:
+        print("Error opening file:" + img_path)
+        return False
+
+def is_multi_frame(img_path):
+    '''
+    A function to tell whether or not an image is animated
+    '''
+    im = Image.open(img_path,'r')
+    test = (hasattr(im,"is_animated") and im.is_animated) or (hasattr(im,"n_frames") and im.n_frames > 1)
+    im.close()
+    return test
+
+def extract_frames(img_path):
+    '''
+    A method to extract each frame of a gif into separate images
+    '''
+    # We do not use copy here as it would only copy the first frame of the gif
+    im = Image.open(img_path,'r')
+    
+    try:
+        while True:
+            print("Saving frame #"+str(im.tell()+1)+" of file: " + str(img_path))
+            # Take frame number and then save a  copy of the current frame
+            frame_num = im.tell()
+            curr_frame = im.copy()
+            # Then we save the frame as a png while converting it to RGBA mode
+            frame_export = Image.new("RGBA",curr_frame.size)
+            frame_export.paste(curr_frame, (0,0), curr_frame.convert("RGBA"))
+            frame_export.save(img_path+"[frame #"+str(frame_num+1)+"].png")
+            frame_export.close()
+            # Then move the image ahead one frame
+            im.seek(im.tell()+1)
+    except EOFError:
+        print("End of Frames")
+        pass
+    im.close()
+
+# **********************************************************************************************************************
+#                                       **** Functions to process colors for files ****
+# **********************************************************************************************************************
+
 def getcolor(img_path):
     '''
     A method to return color info on an image
@@ -72,6 +130,10 @@ def getcolorgif(img_path):
         pass
     return frames
 
+# **********************************************************************************************************************
+#                       **** Functions to "restack" images by sorting the pixels by rgba values ****
+# **********************************************************************************************************************
+
 def stackbyrgba(img_path):
     '''
     Takes an image and then spits out an image with all the rgb values sorted horizontally
@@ -93,58 +155,6 @@ def stackbyrgba(img_path):
     restack.putdata(pixellist)
     restack.save(img_path+"[rgba_stacked].png")
     restack.close()
-
-    
-def stackbyfrequency(original_img):
-    '''
-    Takes an image and then spits out an image with all the rgb values sorted horizontally by frequency
-    '''
-    # Open an image copy and then convert it to RGBA mode
-    im = Image.open(original_img,'r').copy()
-    if im.mode != "RGBA":
-        im = im.convert('RGBA')
-    # Get a list of all colors used from original image
-    colorlist = list(im.getcolors(maxcolors=len(im.getdata())))
-    newpixels = list()
-    # Then sort the pixels by their frequency count in the image for each color
-    for color in sorted(colorlist, key = lambda color: color[0]):
-        newpixels += [color[1]] * color[0]
-
-    # Create a new image with olg image designs and new list of pixels
-    restack = Image.new(im.mode,im.size)
-
-    im.close()
-    
-    restack.putdata(newpixels)
-    restack.save(original_img+"[freq_stacked].png")
-
-    restack.close()
-    
-
-def extract_frames(img_path):
-    '''
-    A method to extract each frame of a gif into separate images
-    '''
-    # We do not use copy here as it would only copy the first frame of the gif
-    im = Image.open(img_path,'r')
-    
-    try:
-        while True:
-            print("Saving frame #"+str(im.tell()+1)+" of file: " + str(img_path))
-            # Take frame number and then save a  copy of the current frame
-            frame_num = im.tell()
-            curr_frame = im.copy()
-            # Then we save the frame as a png while converting it to RGBA mode
-            frame_export = Image.new("RGBA",curr_frame.size)
-            frame_export.paste(curr_frame, (0,0), curr_frame.convert("RGBA"))
-            frame_export.save(img_path+"[frame #"+str(frame_num+1)+"].png")
-            frame_export.close()
-            # Then move the image ahead one frame
-            im.seek(im.tell()+1)
-    except EOFError:
-        print("End of Frames")
-        pass
-    im.close()
 
 def stackbyrgbagif(img_path):
     '''
@@ -184,35 +194,39 @@ def stackbyrgbagif(img_path):
     except EOFError:
         print("End of Frames")
         pass
-    #new_gif = Image.new("RGBA",im.size)
     
     new_gif.save(img_path+"[rgba_restack].gif",save_all=True, append_images=new_frames)
     new_gif.close()
     im.close()
 
-def is_multi_frame(img_path):
+# **********************************************************************************************************************
+#           **** Functions to "restack" images by sorting the pixels by frequency of rgba values ****
+# **********************************************************************************************************************
+
+def stackbyfrequency(original_img):
     '''
-    A function to tell whether or not an image is animated
+    Takes an image and then spits out an image with all the rgb values sorted horizontally by frequency
     '''
-    im = Image.open(img_path,'r')
-    test = (hasattr(im,"is_animated") and im.is_animated) or (hasattr(im,"n_frames") and im.n_frames > 1)
+    # Open an image copy and then convert it to RGBA mode
+    im = Image.open(original_img,'r').copy()
+    if im.mode != "RGBA":
+        im = im.convert('RGBA')
+    # Get a list of all colors used from original image
+    colorlist = list(im.getcolors(maxcolors=len(im.getdata())))
+    newpixels = list()
+    # Then sort the pixels by their frequency count in the image for each color
+    for color in sorted(colorlist, key = lambda color: color[0]):
+        newpixels += [color[1]] * color[0]
+
+    # Create a new image with olg image designs and new list of pixels
+    restack = Image.new(im.mode,im.size)
+
     im.close()
-    return test
-
-def can_open_image(img_path):
-    try:
-        im = Image.open(img_path)
-        im.close()
-        
-    except OSError:
-        print("Error opening file")
-        
-        pass
-    #return False
-        
     
+    restack.putdata(newpixels)
+    restack.save(original_img+"[freq_stacked].png")
 
-        
+    restack.close()
 
 
 process_dict("/test_images")
