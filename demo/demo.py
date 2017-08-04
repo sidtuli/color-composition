843ab3695f3c8d15d116d2c42426cd65d571dc10
sidtuli <stuli@terpmail.umd.edu>
2017-08-03 20:35:31 -0400

    Partial randomization done.

--- demo/demo.py
+++ demo/demo.py
@@ -361,11 +361,133 @@ def randomizepixelsgif(img_path):
     new_gif.close()
     im.close()
 
+def partialrandomizepixels(img_path,proportion):
+    '''
+    Function to randomize a part of an image's pixels
+    '''
+    im = Image.open(img_path,'r').copy()
+    
+    if im.mode != "RGBA":
+        im = im.convert('RGBA')
+    
+    pixellist = list(im.getdata())
+    
+    pixel_num = len(pixellist)
+    pixel_count = 0
+    rand_dict = {}
+    pixel_barrier = pixel_num * proportion
+    rand_arr = []
+    while pixel_count < pixel_barrier:
+        curr = random.randint(0,pixel_num - 1)
+        if curr not in rand_dict:
+            rand_dict[curr] = -1
+            pixel_count += 1
+            rand_arr.append(curr)
+    random.shuffle(rand_arr)
+    i = 0
+    for key in rand_dict:
+        rand_dict[key] = rand_arr[i]
+        temp = pixellist[key]
+        pixellist[key] = pixellist[rand_arr[i]]
+        pixellist[rand_arr[i]] = temp
+        i += 1
+    
+    rand_result = Image.new(im.mode,im.size)
+
+    im.close()
+    
+    rand_result.putdata(pixellist)
+    rand_result.save(img_path+"[part-random].png")
+    rand_result.close()
+
+def partialrandomizepixelsgif(img_path,proportion):
+    # We do not use copy here as it would only copy the first frame of the gif
+    im = Image.open(img_path,'r')
+    # Create gif image object
+    new_gif = Image.new("RGBA",im.size)
+    # Copy first frame from image
+    first_frame = im.copy()
+    first_frame = first_frame.convert("RGBA")
+    # Sort pixels of first frame
+    pixellist = list(first_frame.getdata())
+    
+    pixel_num = len(pixellist)
+    pixel_count = 0
+    rand_dict = {}
+    pixel_barrier = pixel_num * proportion
+    rand_arr = []
+    while pixel_count < pixel_barrier:
+        curr = random.randint(0,pixel_num - 1)
+        if curr not in rand_dict:
+            rand_dict[curr] = -1
+            pixel_count += 1
+            rand_arr.append(curr)
+    random.shuffle(rand_arr)
+    i = 0
+    for key in rand_dict:
+        rand_dict[key] = rand_arr[i]
+        temp = pixellist[key]
+        pixellist[key] = pixellist[rand_arr[i]]
+        pixellist[rand_arr[i]] = temp
+        i += 1
+    
+    # Create object for new first frame of the gif
+    new_first_frame = Image.new("RGBA",im.size)
+    new_first_frame.putdata(pixellist)
+    # Then this new frame is put into the gif image object
+    new_gif.paste(new_first_frame,(0,0),new_first_frame.convert("RGBA"))
+    new_frames = []
+    try:
+        while True:
+            print("Randomizing frame #"+str(im.tell()+1)+" of file: " + str(img_path))
+            # save a  copy of the current frame
+            im.seek(im.tell()+1)
+            curr_frame = im.copy()
+            # Sort the frame's pixels
+            curr_frame = curr_frame.convert('RGBA')
+            
+            pixellist = list(curr_frame.getdata())
+    
+            pixel_num = len(pixellist)
+            pixel_count = 0
+            rand_dict = {}
+            pixel_barrier = pixel_num * proportion
+            rand_arr = []
+            while pixel_count < pixel_barrier:
+                curr = random.randint(0,pixel_num - 1)
+                if curr not in rand_dict:
+                    rand_dict[curr] = -1
+                    pixel_count += 1
+                    rand_arr.append(curr)
+            random.shuffle(rand_arr)
+            i = 0
+            for key in rand_dict:
+                rand_dict[key] = rand_arr[i]
+                temp = pixellist[key]
+                pixellist[key] = pixellist[rand_arr[i]]
+                pixellist[rand_arr[i]] = temp
+                i += 1
+            restack = Image.new("RGBA",curr_frame.size)
+            restack.putdata(pixellist)
+            new_frames.append(restack)
+            # Then move the image ahead one frame
+            
+    except EOFError:
+        print("End of Frames")
+        pass
+    
+    new_gif_info = handle_gif_info(im.info,im.n_frames)
+    new_gif.save(img_path+"[part-random].gif",save_all=True, append_images=new_frames, duration=new_gif_info["duration"], loop=new_gif_info["loop"], background=new_gif_info["background"])
+    new_gif.close()
+    im.close()
+
 # **********************************************************************************************************************
 #                                                       **** Tests ****
 # **********************************************************************************************************************
-randomizepixelsgif(os.getcwd()+"/test_images/dark_souls.gif")
-extract_frames(os.getcwd()+"/test_images/dark_souls.gif[random].gif")
+partialrandomizepixelsgif(os.getcwd()+"/test_images/dark_souls.gif",0.5)
+#partialrandomizepixels(os.getcwd()+"/test_images/Sør-Trøndelag.png",1)
+#randomizepixelsgif(os.getcwd()+"/test_images/dark_souls.gif")
+#extract_frames(os.getcwd()+"/test_images/dark_souls.gif[random].gif")
 #randomizepixels(os.getcwd()+"/test_images/Sør-Trøndelag.png")
 #stackbyfrequency(os.getcwd()+"/test_images/Sør-Trøndelag.png")
 #process_dict("/test_images")
